6600e667d19e38dc1473aad627483da5239c3545
FK <fkrieg.fk11@gmail.com>
2025-06-10 12:29:09 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPlugInDataverse
+{
+    public class DynamicRegexValidationPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Get Plugin Execution Context
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            // Check if operation is Create or Update
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity entity)
+            {
+                string tableName = entity.LogicalName; // Get the table name dynamically
+
+                foreach (var fieldName in entity.Attributes.Keys) // Loop through all fields
+                {
+                    string fieldValue = entity.GetAttributeValue<string>(fieldName);
+
+                    // Retrieve all regex patterns and error messages for this field & table from Dataverse
+                    QueryExpression query = new QueryExpression("ValidationConfig")
+                    {
+                        ColumnSet = new ColumnSet("RegexPattern", "ErrorMessage"),
+                        Criteria = new FilterExpression
+                        {
+                            Conditions =
+                {
+                    new ConditionExpression("FieldName", ConditionOperator.Equal, fieldName),
+                    new ConditionExpression("TableName", ConditionOperator.Equal, tableName)
+                }
+                        }
+                    };
+
+                    EntityCollection results = service.RetrieveMultiple(query);
+
+                    // If no validation rules exist for this field, continue to the next
+                    if (results.Entities.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    // Validate the field against all matching regex rules
+                    foreach (Entity rule in results.Entities)
+                    {
+                        string regexPattern = rule.GetAttributeValue<string>("RegexPattern");
+                        string errorMessage = rule.GetAttributeValue<string>("ErrorMessage");
+
+                        if (!Regex.IsMatch(fieldValue, regexPattern))
+                        {
+                            throw new InvalidPluginExecutionException(errorMessage);
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RegexPlugInDataverse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegexPlugInDataverse")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2a3e33a-86a3-4343-b47a-a7d4102adaee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
